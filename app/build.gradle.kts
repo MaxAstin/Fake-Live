eca76f9d0bb4f73e553745f2c2c1229dbbac01d6
v.podkopaev <max.astin@yandex.ru>
2025-03-25 13:45:51 +0200

    update version up to 3.6.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,8 +19,8 @@ android {
         applicationId = "com.bunbeauty.tiptoplive"
         minSdk = 27
         targetSdk = 35
-        versionCode = 350
-        versionName = "3.5.0"
+        versionCode = 360
+        versionName = "3.6.0"
         multiDexEnabled = true
         setProperty("archivesBaseName", "FakeLive-$versionName")
 
